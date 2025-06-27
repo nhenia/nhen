30ef2931d098bc66d9d8eb7798bf01bb6653eeeb
hereliesaz <hereliesaz@gmail.com>
2025-06-27 18:42:18 -0500

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.androidApplication)
     id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.compose.compiler)
+
 }
 
     android {
@@ -35,6 +37,8 @@ plugins {
             sourceCompatibility = JavaVersion.VERSION_17
             targetCompatibility = JavaVersion.VERSION_17
         }
+
+
         kotlinOptions {
             jvmTarget = "17"
         }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@
         alias(libs.plugins.androidApplication) apply false
         alias(libs.plugins.kotlinAndroid) apply false
         alias(libs.plugins.ksp) apply false
+        id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
     }
 
 
